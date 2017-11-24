567c97027447c6410ce5465bdbde2815c35c1e47
Quentin Dulery <qdulery@gmail.com>
2017-11-24 11:31:30 +0100

    TP3_1.7

--- licence/Web/JQuery/TP3/etudiant/mobilePhone/js/comportement.js
+++ licence/Web/JQuery/TP3/etudiant/mobilePhone/js/comportement.js
@@ -6,6 +6,7 @@ $(document).ready(init);
 
 function init() {
     $("#changer").click(function(){
+        /*
         $.ajax({
             url: 'traitement.php',
             type: 'GET',
@@ -13,5 +14,33 @@ function init() {
                 $("#article").text(data);
             }
         });
+        */
+
+        $.get('num.php',traiterAjax );
+        function traiterAjax(data) {
+            var numero= data;
+            $.get('nom.php',{
+                n : numero
+            }, affichageNom
+            );
+            $.get('Commentaire.php',{
+                    n : numero
+                }, affichageCom
+            );
+            $.get('photo.php',{
+                    n : numero
+                }, affichagePhoto
+            );
+        }
+
+        function affichageNom(data){
+            $('#modele').text(data);
+        }
+        function affichageCom(data){
+            $('#article').text(data);
+        }
+        function affichagePhoto(data){
+            $('#imgPhone').attr("src", "Photos/"+data);
+        }
     });
 }
