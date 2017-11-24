376321351df9c0bd650963a29de34ac8520ff9ad
Quentin Dulery <qdulery@gmail.com>
2017-11-24 10:20:02 +0100

    TP3_js fin

--- licence/Web/JQuery/TP3/etudiant/mobilePhone/js/comportement.js
+++ licence/Web/JQuery/TP3/etudiant/mobilePhone/js/comportement.js
@@ -1,3 +1,17 @@
 /**
  * Created by qdulery on 24/11/2017.
  */
+
+$(document).ready(init);
+
+function init() {
+    $("#changer").click(function(){
+        $.ajax({
+            url: 'traitement.php',
+            type: 'GET',
+            success: function(data){
+                $("#article").text(data);
+            }
+        });
+    });
+}
