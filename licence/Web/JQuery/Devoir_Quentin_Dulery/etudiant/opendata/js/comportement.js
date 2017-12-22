bf910bbfdc1bed9bf06928cdf923e5d21d2aa7e3
Dulery Quentin <qdulery@gmail.com>
2017-12-22 11:18:07 +0100

    Devoir jQuery Maps

--- licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/opendata/js/comportement.js
+++ licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/opendata/js/comportement.js
@@ -1,5 +1,7 @@
 $(document).ready(init());
 
+var map; // Création variable 
+
 function init() {
   var url = "http://api.bandsintown.com/events/search?api_version=2.0&app_id=mon_app&location=la%20rochelle,france&radius=10&format=json";
   envoiAjax(url).done(traitementAjax);
@@ -26,6 +28,13 @@ function traitementAjax(data) {
 function traitementAjax2(data) {
   var ul = $("ul.concert");
 
+  map = new GMaps({   // Création GMaps
+    div: '#map',      // Ou afficher la map
+    lat: 46.160329,
+    lng: -1.1511390000000574,
+    zoom: 12
+  });
+
   $.each(data, function (key, value) {
     var li = $("<li/>").appendTo(ul);
 
@@ -38,5 +47,14 @@ function traitementAjax2(data) {
     $.each(value.artists, function (key, value) {
       li = $("<li/>").text(value.name).appendTo(ol);
     })
+
+    map.addMarker({                      // Fonction ajout marker
+      lat: value.venue.latitude,         // Recuperation latitude
+      lng: value.venue.longitude,       // Recuperation longitude
+      title: value.venue.name,          // Recuperation nom
+      infoWindow: {                     // Info fenetre du marker
+        content: "<a href=\"" + value.venue.url + "\">"+ value.venue.url +"</a>"
+      }
+    });
   })
 }
