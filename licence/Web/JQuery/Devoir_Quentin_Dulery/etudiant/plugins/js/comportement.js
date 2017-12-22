8431c2df033031906509ab0949c892c0c5fce9ec
Dulery Quentin <qdulery@gmail.com>
2017-12-22 10:29:44 +0100

    Exercice 2 Devoir

---
+++ licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/plugins/js/comportement.js
@@ -0,0 +1,23 @@
+$(document).ready(init());
+
+function init() {
+  $("#image").faceDetection({
+    complete: traitementDetection
+  });
+}
+
+function traitementDetection(faces) {
+  var nb = faces.length;
+  $("span.nb").text(nb);
+
+  $.each(faces, function (key, value) {
+    var section = $("section");
+    var div = $("<div/>").appendTo(section);
+    div.css({
+      height: value.height,
+      width: value.width,
+      left: value.x,
+      top: value.y
+    });
+  })
+}
