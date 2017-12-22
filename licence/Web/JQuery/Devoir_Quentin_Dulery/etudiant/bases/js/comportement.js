887a7d204ffb5021ee6123b10d2e5cb77d1af9b4
Dulery Quentin <qdulery@gmail.com>
2017-12-22 09:52:59 +0100

    Devoir jQuery, Exo2

--- licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/bases/js/comportement.js
+++ licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/bases/js/comportement.js
@@ -2,6 +2,8 @@ $(document).ready(init());
 
 function init() {
   exo1();
+  exo2();
+
 }
 
 /**
@@ -10,5 +12,28 @@ function init() {
 function exo1() {
   var liste = $("section#ex1 ul");
   var li = $("<li/>").text("jQuery").appendTo(liste);
-  li.css('background-color', 'grey');
+  li.css('background-color', '#aaa');
+}
+
+/**
+ * Exercice 2
+ */
+function exo2() {
+  $("button.valide").click(actionClick);
+}
+
+function actionClick() {
+  var listeSportifs = $("ul.sportif li");
+  var listeGolf = $("ul.listeGolf");
+  var listeAutre = $("ul.listeAutre");
+
+  listeSportifs.each(function(){
+    var li;
+    if ($(this).attr('class') == 'golf') {
+      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeGolf);
+    }
+    else {
+      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeAutre);
+    }
+  })
 }
