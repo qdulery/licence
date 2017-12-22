e22fa3439eca2cfc3a68bd341eb9571fbbc8b178
Dulery Quentin <qdulery@gmail.com>
2017-12-22 10:17:08 +0100

    Exo 3

--- licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/bases/js/comportement.js
+++ licence/Web/JQuery/Devoir_Quentin_Dulery/etudiant/bases/js/comportement.js
@@ -3,7 +3,7 @@ $(document).ready(init());
 function init() {
   exo1();
   exo2();
-
+  exo3();
 }
 
 /**
@@ -12,7 +12,7 @@ function init() {
 function exo1() {
   var liste = $("section#ex1 ul");
   var li = $("<li/>").text("jQuery").appendTo(liste);
-  li.css('background-color', '#aaa');
+  li.css('background-color', 'grey');
 }
 
 /**
@@ -30,10 +30,65 @@ function actionClick() {
   listeSportifs.each(function(){
     var li;
     if ($(this).attr('class') == 'golf') {
-      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeGolf);
+      li = $("<li/>").text($(this).text());
+      li.addClass($(this).attr('class'));
+      li.appendTo(listeGolf); 
     }
     else {
-      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeAutre);
+      li = $("<li/>").text($(this).text());
+      li.addClass($(this).attr('class'));
+      li.appendTo(listeAutre);
     }
   })
+}
+
+/**
+ * Exercice 3
+ */
+function exo3() {
+  $("button.btnExo3").click(actionClickEx3);
+}
+
+function actionClickEx3() {
+  var url = "exo3.php";
+  envoiAjax(url).done(traitementSportifs);
+}
+
+function traitementSportifs(data) {
+  var h3 = $("section#ex3 h3");
+  var description = data.description;
+  h3.text(description);
+
+  var tableau = $("<table/>");
+  $("section#ex3").append(tableau);
+
+  var tr = $("<tr/>").appendTo(tableau);
+  var th = $("<th/>").text("Nom").appendTo(tr);
+  th = $("<th/>").text("Gain").appendTo(tr);
+  th = $("<th/>").text("Sport").appendTo(tr);
+
+  $.each(data.infos, function (key, value) {
+    var nom = value.nom;
+    var gain = value.gain;
+    var sport = value.sport;
+
+    tr = $("<tr/>").appendTo(tableau);
+    var td = $("<td/>").text(nom).appendTo(tr);
+    td = $("<td/>").text(gain).appendTo(tr);
+    td = $("<td/>").text(sport).appendTo(tr);
+  })
+}
+
+function envoiAjax(url){
+  return $.ajax({
+      url: url,
+      type: "post",
+      dataType: "JSON",
+      data: {
+        sportif: 2017
+      }
+  }).fail(function(){
+      var erreur = $("section#ex3 h3"); // On récupère la liste
+      var afficheErreur = $("<p/>").text("Erreur !").appendTo(erreur); // Ajout du texte au li et ajout du li à la liste
+  });
 }
